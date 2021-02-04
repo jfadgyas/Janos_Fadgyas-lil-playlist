1727c886644c3034d4386646274414682f9b1df1
jfadgyas <djdeville72@yahoo.com>
2021-02-04 23:24:19 +0100

    redux counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './Actions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const counter = useSelector(state => state.counter)
+    const isLoggedIn = useSelector(state => state.isLoggedIn)
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+            <h1>Counter: {counter}</h1>
+            <button onClick={() => dispatch(increment(2))}>+</button>
+            <button onClick={() => dispatch(decrement(0.5))}>-</button>
+            {isLoggedIn && <h3>Secret info</h3>}
+        </div>
+    )
 }
 
 export default App;
